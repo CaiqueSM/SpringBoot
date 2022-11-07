4083d7d0450465248757b041833ad5a17430bee8
caimachado <caique9machado@gmail.com>
2022-11-07 10:47:50 -0300

    Criando EndPoint FindAll

--- src/main/java/com/api/springboot/api/services/UserService.java
+++ src/main/java/com/api/springboot/api/services/UserService.java
@@ -2,6 +2,9 @@ package com.api.springboot.api.services;
 
 import com.api.springboot.api.domain.User;
 
+import java.util.List;
+
 public interface UserService {
     User findById(Integer id);
+    List<User> findAll();
 }
--- src/main/java/com/api/springboot/api/services/UserServiceImpl.java
+++ src/main/java/com/api/springboot/api/services/UserServiceImpl.java
@@ -6,6 +6,7 @@ import com.api.springboot.api.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -17,5 +18,9 @@ public class UserServiceImpl implements UserService{
     public User findById(Integer id) {
         Optional<User> obj = repository.findById(id);
         return obj.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado."));
+    }
+
+    public List<User> findAll(){
+        return repository.findAll();
     }
 }
